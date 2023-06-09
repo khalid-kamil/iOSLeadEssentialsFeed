d70df69ab40d0c6bf61b2a65c7e1ca0432d53909
Khalid Kamil <khalidkamil28@gmail.com>
2023-06-09 21:00:39 +0100

    From singletons and globals to proper dependency injection. Networking module start. Renamed project to EssentialFeed.

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Singleton
+//
+//  Created by Khalid Kamil on 09/06/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
