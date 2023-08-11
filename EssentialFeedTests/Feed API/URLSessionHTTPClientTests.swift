addc87969e694d42913d53904acb1d042580192b
Khalid Kamil <khalidkamil28@gmail.com>
2023-08-11 19:34:56 +0100

    Extract test values creation to shared factory methods

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -53,7 +53,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
   }
 
   func test_getFromURL_failsOnRequestError() {
-    let requestError = NSError(domain: "any error", code: 1)
+    let requestError = anyNSError()
 
     let receivedError = resultErrorFor(data: nil, response: nil, error: requestError) as NSError?
 
