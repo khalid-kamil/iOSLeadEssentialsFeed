313fcd029f474a9acf8b3a39114b6e2e814d1538
Khalid Kamil <khalidkamil28@gmail.com>
2024-01-31 12:30:45 +0000

    Move FeedImageDataLoader into its new file

---
+++ EssentialFeed/EssentialFeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Khalid Kamil on 31/01/2024.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
