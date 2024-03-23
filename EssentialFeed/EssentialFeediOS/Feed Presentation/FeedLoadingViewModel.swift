e149b1e468bde205cf2161efc6805537f339e296
Khalid Kamil <khalidkamil28@gmail.com>
2024-03-23 03:33:52 +0000

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Khalid Kamil on 23/03/2024.
+//
+
+import EssentialFeed
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
