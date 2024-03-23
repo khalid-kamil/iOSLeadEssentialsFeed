6a922218935cee04019b7c095146809f5d611caf
Khalid Kamil <khalidkamil28@gmail.com>
2024-03-23 03:25:26 +0000

    Add FeedImagePresenter holding FeedImage presentation logic (including image loading state)

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedImageViewModel.swift
@@ -7,55 +7,14 @@
 
 import EssentialFeed
 
-public final class FeedImageViewModel<Image> {
-    typealias Observer<T> = (T) -> Void
-
-    private var task: FeedImageDataLoaderTask?
-    private var model: FeedImage
-    private var imageLoader: FeedImageDataLoader
-    private var imageTransformer: (Data) -> Image?
-
-    init(model: FeedImage, imageLoader: FeedImageDataLoader, imageTransformer: @escaping (Data) -> Image?) {
-        self.model = model
-        self.imageLoader = imageLoader
-        self.imageTransformer = imageTransformer
-    }
-
-    var location: String? {
-        model.location
-    }
-
+struct FeedImageViewModel<Image> {
+    let description: String?
+    let location: String?
+    let image: Image?
+    let isLoading: Bool
+    let shouldRetry: Bool
+    
     var hasLocation: Bool {
         location != nil
-    }
-
-    var description: String? {
-        model.description
-    }
-
-    var onImageLoad: Observer<Image>?
-    var onImageLoadingStateChange: Observer<Bool>?
-    var onShouldRetryImageLoadStateChange: Observer<Bool>?
-
-    func loadImageData() {
-        onImageLoadingStateChange?(true)
-        onShouldRetryImageLoadStateChange?(false)
-        task = imageLoader.loadImageData(from: model.url) { [weak self] result in
-            self?.handle(result)
-        }
-    }
-
-    private func handle(_ result: FeedImageDataLoader.Result) {
-        if let image = (try? result.get()).flatMap(imageTransformer) {
-            onImageLoad?(image)
-        } else {
-            onShouldRetryImageLoadStateChange?(true)
-        }
-        onImageLoadingStateChange?(false)
-    }
-
-    func cancelImageDataLoad() {
-        task?.cancel()
-        task = nil
     }
 }
