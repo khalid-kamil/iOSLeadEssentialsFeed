bc0cf41c731050344e18edd9fa4e3e2552294d9c
Khalid Kamil <khalidkamil28@gmail.com>
2024-04-20 01:24:42 +0100

    Move `ErrorView` to a separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Khalid Kamil on 20/04/2024.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
