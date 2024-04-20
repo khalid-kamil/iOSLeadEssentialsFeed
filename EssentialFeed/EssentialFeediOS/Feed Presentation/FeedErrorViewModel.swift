fb5fe46ba4a701c67962e41abf62e4ae9707c4aa
Khalid Kamil <khalidkamil28@gmail.com>
2024-04-20 15:56:14 +0100

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Khalid Kamil on 20/04/2024.
+//
+
+import EssentialFeed
+
+struct FeedErrorViewModel {
+    let message: String?
+}
